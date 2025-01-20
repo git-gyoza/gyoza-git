e04c1ac68315958c8e2c3feb8bfe003c15849783
user <fulminazzo21@gmail.com>
2025-01-20 23:59:56 +0100

    Moved MockGyozaServer and derivatives to gyoza_server_test.js for incompatibilities with Jest

--- __tests__/gyoza_server_test.js
+++ __tests__/gyoza_server_test.js
@@ -1,4 +1,4 @@
-const { MockGyozaServer, MockResponse } = require('mock_gyoza_server')
+const GyozaServer = require("../src/gyoza_server");
 
 describe('GyozaServer tests', () => {
 
@@ -11,3 +11,59 @@ describe('GyozaServer tests', () => {
     })
 
 })
+
+/**
+ * A mock for the actual {@link GyozaServer}.
+ * Provides a {@link MockGyozaServer#handleRequest}
+ * method for testing purposes.
+ */
+class MockGyozaServer extends GyozaServer {
+
+    /**
+     * Makes the protected method {@link GyozaServer#_handleRequest}
+     * publicly available.
+     *
+     * @param method the request method ('GET' by default)
+     * @param path the request path ('' by default)
+     * @param headers the request headers (empty be default)
+     * @returns {MockResponse} the response containing all the data sent
+     */
+    handleRequest(method = 'GET', path = '', headers = {}) {
+        const request = {
+            'method': method,
+            'url': path,
+            'headers': headers,
+        }
+        const response = new MockResponse()
+        super._handleRequest(request, response)
+        return response
+    }
+
+}
+
+/**
+ * A mock for the actual response object offered
+ * by the {@link http} module.
+ */
+class MockResponse {
+    statusCode;
+    headers;
+    body;
+    ended = false;
+
+    writeHead(statusCode, headers) {
+        this.statusCode = statusCode;
+        this.headers = headers;
+    }
+
+    write(body) {
+        this.body = body;
+    }
+
+    end() {
+        this.ended = true;
+    }
+
+}
+
+module.exports = { MockGyozaServer, MockResponse };
