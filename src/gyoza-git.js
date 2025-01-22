b35311486a2bd0ad859ce63c54a4408cb4e386f8
user <fulminazzo21@gmail.com>
2025-01-22 01:07:52 +0100

    Added several static constants

--- src/gyoza-git.js
+++ src/gyoza-git.js
@@ -3,7 +3,10 @@ const fs = require('fs')
 pkg = JSON.parse(fs.readFileSync(`${__dirname}/${'../package.json'}`, 'utf8'))
 
 module.exports = {
+    NAME: pkg['name'],
+    DESCRIPTION: pkg['description'],
     VERSION: pkg['version'],
+    REPOSITORIES_DIRECTORY_ENV_NAME: 'REPOSITORIES_DIRECTORY',
     SERVER_NAME: `${pkg['name']}/${pkg['version']}`,
     DEFAULT_PORT: 22125
 }
